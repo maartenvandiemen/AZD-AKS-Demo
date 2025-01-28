c2b9917628fac8a4a57450ac6fc178af73b36d75
Maarten van Diemen <maartenvandiemen@msn.com>
2025-01-28 17:37:40 +0100

    Fix AZD down

--- azd-hooks/predown.ps1
+++ azd-hooks/predown.ps1
@@ -1,8 +1,10 @@
 #!/usr/bin/env pwsh
 
 try {
-    az group delete --name ${env:AZURE_RG_NAME} --yes
+    az tag update --resource-id /subscriptions/${env:AZURE_SUBSCRIPTION_ID}/resourceGroups/MA_${env:AZURE_MONITOR_NAME}_${env:AZURE_LOCATION}_managed --operation delete --tags azd-env-name=${env:AZURE_ENV_NAME}
+    az tag update --resource-id /subscriptions/${env:AZURE_SUBSCRIPTION_ID}/resourceGroups/MA_${env:AZURE_MONITOR_NAME}_${env:AZURE_LOCATION}_managed/providers/Microsoft.Insights/dataCollectionEndpoints/${env:AZURE_MONITOR_NAME} --operation delete --tags azd-env-name=${env:AZURE_ENV_NAME}
+    az tag update --resource-id /subscriptions/${env:AZURE_SUBSCRIPTION_ID}/resourceGroups/MA_${env:AZURE_MONITOR_NAME}_${env:AZURE_LOCATION}_managed/providers/Microsoft.Insights/dataCollectionRules/${env:AZURE_MONITOR_NAME} --operation delete --tags azd-env-name=${env:AZURE_ENV_NAME}
 } catch {
-    Write-Error "Failed to get AKS credentials: $_"
+    Write-Error "Failed to remove tags. If these tags can't be removed AZD Down will not be able to execute"
     exit 1
 }
